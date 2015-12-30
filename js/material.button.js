d46c4bd073bc7a787aefb0cd5bf67d0dcfb091dd
AetherXCloud <epicpotatoe3@gmail.com>
2015-12-30 14:26:49 +1000

    Fixed button replacement and added handler
    No Further Comment

--- js/material.button.js
+++ js/material.button.js
@@ -1,8 +1,12 @@
 /*  --  Replaces discord buttons with MD buttons  --  */
-
-$('btn btn-primary span').each(function( index ) {
-	var buttontext = this.html;
-	this.attr('style', 'display: none;');
-	this.parent().append('<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">' + buttontext + '</button>');
+$( document ).ready(function() {
+	$('btn btn-primary span').each(function() {
+		var buttontext = $(this).html;
+		var reactid = $(this).attr('data-reactid');
+		$(this).attr('style', 'display: none;');
+		$(this).parent().append('<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored mc-react-btn">' + buttontext + '</button>');
+	});
+	$( "button .mc-react-btn" ).on( "click", function() {
+		$(this).prev("button").click();
+	});
 });
-
