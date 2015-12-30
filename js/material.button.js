5b10710a1f8d84c4fa519a4c3706976b0133e320
AetherXCloud <epicpotatoe3@gmail.com>
2015-12-30 14:49:24 +1000

    added Mutation Observer for setings

--- js/material.button.js
+++ js/material.button.js
@@ -1,3 +1,22 @@
+var targetNodes         = $('span[data-reactid=".0.5"]');
+var MutationObserver    = window.MutationObserver || window.WebKitMutationObserver;
+var myObserver          = new MutationObserver (mutationHandler);
+var obsConfig           = { childList: true, characterData: true, attributes: true, subtree: true };
+
+//--- Add a target node to the observer. Can only add one node at a time.
+targetNodes.each ( function () {
+    myObserver.observe (this, obsConfig);
+} );
+
+function mutationHandler (mutationRecords) {
+	$('btn btn-primary span').each(function() {
+		var buttontext = $(this).html;
+		var reactid = $(this).attr('data-reactid');
+		$(this).attr('style', 'display: none;');
+		$(this).parent().append('<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored mc-react-btn">' + buttontext + '</button>');
+	});
+}
+
 /*  --  Replaces discord buttons with MD buttons  --  */
 $( document ).ready(function() {
 	$('btn btn-primary span').each(function() {
